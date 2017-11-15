e8533d034b0d5ed83c7fa7c3069a1a4644b6e0da
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-11-15 16:53:26 +0100

    feat: Add method to get and update book shelf

--- src/App.js
+++ src/App.js
@@ -10,6 +10,40 @@ class BooksApp extends React.Component {
     books: []
   };
 
+  handleCategory = bookID => {
+    let { books } = this.state;
+    let book = books.find(value => {
+      return value.id === bookID;
+    });
+    if (book) {
+      return book.shelf;
+    } else {
+      return "none";
+    }
+  };
+
+  handleUpdateBookRack = (book, rack) => {
+    let { books } = this.state;
+    BooksAPI.update(book, rack)
+      .then(shelves => {
+        let updatedBook = books.find(book => {
+          return book.id === book.id;
+        });
+        if (!updatedBook) {
+          book.shelf = rack;
+          books.push(book);
+        } else {
+          updatedBook.shelf = rack;
+        }
+        this.setState(state => ({
+          books: books
+        }));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   componentDidMount() {
     BooksAPI.getAll().then(books =>
       this.setState(state => ({
@@ -23,8 +57,22 @@ class BooksApp extends React.Component {
     const { books } = this.state;
     return (
       <div className="app">
-        <Route exact path="/search" render={() => <SearchBook />} />
-        <Route exact path="/" render={() => <BookList books={books} />} />
+        <Route
+          exact
+          path="/search"
+          render={() => <SearchBook books={books} />}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <BookList
+              bookCategory={this.handleCategory}
+              updateCategory={this.handleUpdateBookRack}
+              books={books}
+            />
+          )}
+        />
       </div>
     );
   }
