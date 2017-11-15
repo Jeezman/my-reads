23c04f8106628dd30366a7cf4b30f0989d687f04
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-11-15 15:02:36 +0100

    feat: Update book listing component
    - Add component to handle book sections
    - Add Book component

---
+++ src/components/BookRack.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Book from "./Book";
+import "../App.css";
+
+class BookRack extends React.Component {
+  render() {
+    const { title, books } = this.props;
+    return (
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{title}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {books.map(book => (
+                <li key={book.id}>
+                  <Book book={book} />
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookRack;
